7f54b0ef465268b5e3e3dab3da4ccd8b4f8fb945
OyakXD <kayovieira157@gmail.com>
2024-05-03 12:16:41 -0300

    feat: Finalizando o projeto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 package src;
 
-import src.br.com.dio.desafio.dominio.Conteudo;
-import src.br.com.dio.desafio.dominio.Curso;
-import src.br.com.dio.desafio.dominio.Mentoria;
+import src.br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -14,7 +12,6 @@ public class Main {
         curso.setDescricao("Aprenda os conceitos básicos de Java");
         curso.setCargaHoraria(8);
 
-
         System.out.println(curso);
 
         Mentoria mentoria = new Mentoria();
@@ -24,6 +21,26 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("GFT START #2 Java");
+        bootcamp.setDescricao("Bootcamp para formação de desenvolvedores Java");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Lucas");
+        dev.inscreverBootcamp(bootcamp);
+        dev.progredir();
+
+        System.out.println("Conteudos Inscrito" + dev.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos" + dev.getConteudosConcluidos());
+        System.out.println("XP: " + dev.calcularTotalXp());
+
+        // ...
+
+
 
     }
 }
